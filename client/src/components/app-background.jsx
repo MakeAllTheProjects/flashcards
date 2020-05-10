f5fed6dd1adddd95ee3ef184fd977360a931bdb1
Meghan Bomberger <meghanbomberger@gmail.com>
2020-05-10 16:03:36 -0600

    reversed context error

--- client/src/components/app-background.jsx
+++ client/src/components/app-background.jsx
@@ -4,11 +4,53 @@ import React, {
 	useEffect,
 	useContext
 } from 'react'
-import { ThemeContext } from '../utils/context/theme-context'
 
 export default function AppBackground ({children}) {
-	const { lineCount, windowRatio } = useContext(ThemeContext)
+	const defaultPaperRatio = 0.7727
+	const [lineCount, setLineCount] = useState(33)
+	const [windowRatio, setWindowRatio] = useState(defaultPaperRatio)
+	const [windowHeight, setWindowHeight] = useState(window.innerHeight)
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+	const [paperHeight, setPaperHeight] = useState(window.innerHeight)
+	const [paperWidth, setPaperWidth] = useState(window.innerWidth)
 	const [lines, setLines] = useState([])
+
+	const updateWindowSize = () => {
+		setWindowHeight(window.innerHeight)
+		setWindowWidth(window.innerWidth)
+		setWindowRatio(window.innerWidth / window.innerHeight)
+	}
+
+	function updatePaperSize() {
+		if (windowRatio > defaultPaperRatio) {
+			setPaperHeight(windowWidth * 1.294117647058824)
+			setPaperWidth(windowWidth)
+		} else if (windowRatio > defaultPaperRatio) {
+			setPaperHeight(windowWidth)
+			setPaperWidth(windowWidth)
+		}
+	}
+
+	function updateLineCount() {
+		if (windowRatio > defaultPaperRatio) {
+			setLineCount(Math.round(33 / windowRatio))
+		}
+	}
+
+	useEffect(() => {
+		window.addEventListener("resize", updateWindowSize)
+		return () => window.removeEventListener("resize", updateWindowSize)
+	})
+
+	useEffect(() => {
+		updatePaperSize()
+		updateLineCount()
+		return () => {
+			setPaperHeight(windowHeight)
+			setPaperWidth(windowWidth)
+			setLineCount(33)
+		}
+	}, [windowRatio])
 
 	useEffect(() => {
 		const newLines = []
