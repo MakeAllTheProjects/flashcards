096ba1b3dcd5dc65e4fd6e2c3d8289cef32c480c
Meghan Bomberger <meghanbomberger@gmail.com>
2020-05-10 16:10:52 -0600

    removed redundent code from App.js for background managment

--- client/src/App.js
+++ client/src/App.js
@@ -5,61 +5,6 @@ import './App.scss'
 import AppBackground from './components/app-background'
 
 export default function App () {
-	const [windowHeight, setWindowHeight] = useState(window.innerHeight)
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth)
-	const [windowRatio, setWindowRatio] = useState(window.innerWidth / window.innerHeight)
-	const [paperHeight, setPaperHeight] = useState(window.innerHeight)
-	const [paperWidth, setPaperWidth] = useState(window.innerWidth)
-	const [lineCount, setLineCount] = useState(33)
-	const [lines, setLines] = useState([])
-
-	const defaultPaperRatio = 0.7727
-
-	const updateWindowSize = () => {
-		setWindowHeight(window.innerHeight)
-		setWindowWidth(window.innerWidth)
-		setWindowRatio(window.innerWidth/window.innerHeight)
-	}
-
-	function updatePaperSize () {
-		if (windowRatio > defaultPaperRatio) {
-			setPaperHeight(windowWidth * 1.294117647058824)
-			setPaperWidth(windowWidth)
-		} else if (windowRatio > defaultPaperRatio) {
-			setPaperHeight(windowWidth)
-			setPaperWidth(windowWidth)
-		}
-	}
-
-	function updateLineCount () {
-		if (windowRatio > defaultPaperRatio) {
-			setLineCount(Math.round(33/windowRatio))
-		}
-	}
-
-	useEffect(() => {
-		window.addEventListener("resize", updateWindowSize)
-		return () => window.removeEventListener("resize", updateWindowSize)
-	})
-
-	useEffect(() => {
-		updatePaperSize()
-		updateLineCount()
-		return () => {
-			setPaperHeight(windowHeight)
-			setPaperWidth(windowWidth)
-			setLineCount(33)
-		}
-	}, [windowRatio])
-
-	useEffect(() => {
-		const newLines = []
-		_.times(lineCount, (i) => {
-			newLines.push(<div className='blue-line' key={`blue-line-${i+1}`} />)
-		})
-		setLines(newLines)
-	}, [lineCount])
-
 	return (
 		<AppBackground>
 			<div className='app-grid'>
