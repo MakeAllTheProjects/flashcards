6c03a4792acfbf97ca7df7fa2c3efca264cb791b
Meghan Bomberger <meghanbomberger@gmail.com>
2020-08-23 18:50:10 -0600

    card flip with question and answer display added to card library ui

--- client/src/components/cards/card.jsx
+++ client/src/components/cards/card.jsx
@@ -2,10 +2,22 @@ import React from 'react'
 
 import './card.scss'
 
-export default function Card () {
+export default function Card (props) {
+  const [isFlipped, setIsFlipped] = React.useState(false)
+  
   return (
-    <div className='card-container'>
-      this is a card
+    <div
+      className={isFlipped ? "card flipped" : "card"}
+      onClick={() => setIsFlipped(!isFlipped)}
+    >
+      <div className="card-content">
+        <div className="card-front">
+          {props.question}
+        </div>
+        <div className="card-back">
+          {props.answer}
+        </div>
+      </div>
     </div>
   )
 }
