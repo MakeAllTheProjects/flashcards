f902b21408d1631939c46e7cae9c69f31920fa8d
Meghan Bomberger <meghanbomberger@gmail.com>
2021-01-28 00:01:35 -0700

    card library loading spinner

--- client/src/components/pages/card-library/CardLibrary.jsx
+++ client/src/components/pages/card-library/CardLibrary.jsx
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import { GlobalContext } from '../../../App'
 import Card from './Card'
 import Header from '../../header/Header'
+import Loading from '../../Loading'
 import './CardLibrary.scss'
 import ViewCardsIcon from '../../../assets/svg/sketch-style/018-layers.svg'
 
 export default function CardLibrary () {
 	const context = useContext(GlobalContext)
 	const { state, dispatch } = context
+	const [ visibleCards, setVisibleCards ] = useState([])
+	const [ isLoading, setIsLoading ] = useState(true)
+
+	useEffect(() => {
+		setVisibleCards(state.cards)
+		setIsLoading(false)
+	}, [state.cards])
 
 	return (
 		<div className="page card-library-page">
@@ -17,7 +25,13 @@ export default function CardLibrary () {
 				cornerIcon={ViewCardsIcon}
 			/>
 			<main className="main card-library-container">
-				{state.cards.length > 0 && state.cards.map(card => (
+				{isLoading && (
+					<>
+						<p className="loading-library">Loading Card Library...</p>
+						<Loading/>
+					</>
+				)}
+				{!isLoading && visibleCards.length > 0 && visibleCards.map(card => (
 					<Card
 						key={card.id}
 						card={card}
